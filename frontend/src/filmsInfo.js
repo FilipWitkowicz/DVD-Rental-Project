d9470e62eb30ecdb38d6bc66d3431396ca96cf31
Fiflon <odzera.dotrofera@gmail.com>
2024-04-12 21:53:33 +0200

    Added filmsInfo with db silmulation and film posters

---
+++ frontend/src/filmsInfo.js
@@ -0,0 +1,74 @@
+export default {
+  movies: [
+    {
+      id: 1,
+      film: {
+        title: "Her",
+        image: "frontend/src/img/image_1.png",
+        genre: "drama", //moze lista bo film moze miec kilka garunkow?
+        description:
+          "In future Los Angeles, Theodore (Phoenix) is a lonely man who has had his heart broken. He uses his savings to invest in a personalised computer operating system called Samantha (voiced by Johansson), who is designed to meet his every requirement.",
+      },
+    },
+    {
+      id: 2,
+      film: {
+        title: "Cars",
+        image: "frontend/src/img/image_2.png",
+        genre: "family",
+        description:
+          "The movie is about Lightning McQueen, a racecar who is on his way to California where he is scheduled to race for the Piston Cup. He gets lost on his way and finds himself in Radiator Springs, a fictional town on Route 66.",
+      },
+    },
+    {
+      id: 3,
+      film: {
+        title: "Seven",
+        image: "frontend/src/img/image_3.png",
+        genre: "thriller",
+        description:
+          "Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives.",
+      },
+    },
+    {
+      id: 4,
+      film: {
+        title: "Lost in transition",
+        image: "frontend/src/img/image_4.png",
+        genre: "drama",
+        description:
+          "A lonely, aging movie star named Bob Harris (Bill Murray) and a conflicted newlywed, Charlotte (Scarlett Johansson), meet in Tokyo. Bob is there to film a Japanese whiskey commercial; Charlotte is accompanying her celebrity-photographer husband.",
+      },
+    },
+    {
+      id: 5,
+      film: {
+        title: "The Amazing Spider-Man 2",
+        image: "frontend/src/img/image_5.png",
+        genre: "action",
+        description:
+          "When New York is put under siege by Oscorp, it is up to Spider-Man to save the city he swore to protect as well as his loved ones.",
+      },
+    },
+    {
+      id: 6,
+      film: {
+        title: "(500) Days of Summer",
+        image: "frontend/src/img/image_6.png",
+        genre: "comedy",
+        description:
+          "After being dumped by the girl he believes to be his soulmate, hopeless romantic Tom Hansen reflects on their relationship to try and figure out where things went wrong and how he can win her back.",
+      },
+    },
+    {
+      id: 7,
+      film: {
+        title: "Ford v Ferrari",
+        image: "frontend/src/img/image_7.png",
+        genre: "action",
+        description:
+          "American car designer Carroll Shelby and driver Ken Miles battle corporate interference and the laws of physics to build a revolutionary race car for Ford in order to defeat Ferrari at the 24 Hours of Le Mans in 1966. ",
+      },
+    },
+  ],
+};
