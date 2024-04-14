6a13d8008a8cfcfd2b5b1ecbf66a2c0de85c11ee
Filip Witkowicz <filip.witkowicz1@gmail.com>
2024-04-14 17:57:31 +0200

    Auth. version: poor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,22 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import YourFilms from './pages/YourFilms';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import ProtectedRoute from "./components/ProtectedRoute"
 
 import './styles/App.scss';
+
+function Logout() {
+    localStorage.clear()
+    return <Navigate to="/login" />
+  }
+  
+  function RegisterAndLogout() {
+    localStorage.clear()
+    return <Register />
+  }
 
 const App = () => {
     return (
@@ -21,16 +35,33 @@ const App = () => {
                     />
                     <Route 
                         path='/home'
-                        element={<Home />} 
+                        element={
+                            <ProtectedRoute>
+                                <Home />
+                            </ProtectedRoute>
+                        } 
                     />
                     <Route 
                         path='/profile'
-                        element={<Profile />}
+                        element={
+                            <ProtectedRoute>
+                                <Profile />
+                            </ProtectedRoute>
+                        }
                     />
                     <Route 
                         path='/yourfilms'
-                        element={<YourFilms />}
+                        element={
+                            <ProtectedRoute>
+                                <YourFilms />
+                            </ProtectedRoute>
+                        }
                     />
+                
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="/register" element={<RegisterAndLogout />} />
+
                     <Route 
                         path='*'
                         element={<h1>404. Not found</h1>} 
