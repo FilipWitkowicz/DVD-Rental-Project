e023afe36ececa26e9532e5796908bc66591bc96
Filip Witkowicz <filip.witkowicz1@gmail.com>
2024-04-16 17:40:24 +0200

    semi-collon adjustment :DDDDDD

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,7 +3,6 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/App.scss"
-//import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("");
@@ -50,7 +49,6 @@ function Form({ route, method }) {
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
             />
-            {loading && <LoadingIndicator />}
             <button className="form-button" type="submit">
                 {name}
             </button>
